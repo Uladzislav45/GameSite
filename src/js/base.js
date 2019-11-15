74109075c760870942830270a7c0dc5acceee71c
Uladzislav45 <uladislav.stankevich@gmail.com>
2019-11-15 21:07:12 +0300

    Добавил фото и карсулесь

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,28 @@
-console.log('Привет');
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+  setInterval(function(){
+plusSlide();
+  }, 2000);
