b1d02f490d9eeee094c58626fe338ebd69f8b20a
Uladzislav45 <uladislav.stankevich@gmail.com>
2019-11-23 02:29:13 +0300

    +++

--- src/js/base.js
+++ src/js/base.js
@@ -2,46 +2,52 @@
 
 var form = document.querySelector('.contact-form');
 var requireInputs = document.querySelectorAll('.require');
-
-form.addEventListener('submit', function(event){
+var messeg = document.getElementsByClassName('.messeg-none');
+debugger
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     removeAlerts();
     checkInputs();
+
 });
 
 function checkInputs() {
-    var emptyInputs = 0; 
-      for (var i = 0; i < requireInputs.length; i++) {
-          if(!requireInputs[i].value) {
-           emptyInputs++;
-           var errorMsg = document.createElement('span');
-           errorMsg.classList.add('help-block');
-           errorMsg.innerHTML = 'Enter text'; 
-           requireInputs[i].classList.add('errorMsg');
-           requireInputs[i].parentElement.insertBefore(errorMsg, null);
-          } 
-             }
-             if (emptyInputs === 0) {
-             return true; 
-           } else {
-               return false;
-           }
-   }
-   function removeAlerts() {
+    var emptyInputs = 0;
+    for (var i = 0; i < requireInputs.length; i++) {
+        if (!requireInputs[i].value) {
+            emptyInputs++;
+            var errorMsg = document.createElement('span');
+            errorMsg.classList.add('help-block');
+            errorMsg.innerHTML = 'Enter text';
+            requireInputs[i].classList.add('errorMsg');
+            requireInputs[i].parentElement.insertBefore(errorMsg, null);
+        }
+       else{
+        var messeg = document.getElementById('messeg');
+        messeg.className = 'messeg';
+        setInterval(function(){
+            messeg.className = 'messeg-none';
+        }, 4000);
+       }
+    }
+    if (emptyInputs === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function removeAlerts() {
     var alerts = document.querySelectorAll('.help-block');
-for(var i = 0; i < alerts.length; i++) {
-    alerts[i].remove();
-}
-for(var i = 0; i < requireInputs.length; i++) {
-    requireInputs[i].classList.remove('errorMsg');
-}
+    for (var i = 0; i < alerts.length; i++) {
+        alerts[i].remove();
+    }
+    for (var i = 0; i < requireInputs.length; i++) {
+        requireInputs[i].classList.remove('errorMsg');
+    }
 }
 
 
 
 
 //gallery
-
-
-
-
